b3e30bd8cbc1999685b7de99cdab77cc54bc3234
Andrey Petrov <andrey.petrov@shazow.net>
2011-10-25 14:56:06 -0700

    Adding untested jinja2 support.

--- composer/filters.py
+++ composer/filters.py
@@ -22,8 +22,15 @@ try:
 except ImportError:
     docutils = False
 
+try:
+    import jinja2
+except ImportError:
+    jinja2 = False
 
-__all__ = ['Filter', 'Markdown', 'Mako', 'MakoContainer', 'RestructuredText']
+
+__all__ = ['Filter',
+           'Mako', 'MakoContainer', 'Jinja2',
+           'RestructuredText','Markdown']
 
 
 class Filter(object):
@@ -60,18 +67,14 @@ class Mako(Filter):
 
         super(Mako, self).__init__(index)
 
-        from mako.lookup import TemplateLookup
-
         kw = dict(input_encoding='utf-8', output_encoding='utf-8', encoding_errors='replace')
         kw.update(template_kw)
 
         self.template_kw = kw
-        self.lookup = TemplateLookup(**self.template_kw)
-
+        self.lookup = mako.lookup.TemplateLookup(**self.template_kw)
 
     def __call__(self, content, route=None):
-        from mako.template import Template
-        t = Template(content, lookup=self.lookup, input_encoding='utf-8', output_encoding='utf-8', encoding_errors='replace')
+        t = mako.template.Template(content, lookup=self.lookup, **self.template_kw)
 
         return str(t.render(index=self.index, route=route))
 
@@ -96,9 +99,14 @@ class MakoContainer(Mako):
 
 
 class RestructuredText(Filter):
+    # FIXME: This is untested and probably not Best Practices compliant. Someone
+    # who seriously uses RST should make this filter better.
+
     def __init__(self, index, **rst_kw):
         if not docutils:
             raise ImportError("RestructuredText filter requires the 'docutils' package to be installed.")
+
+        super(RestructuredText, self).__init__(index)
 
         self.rst_kw = rst_kw
 
@@ -106,8 +114,37 @@ class RestructuredText(Filter):
         return docutils.core.publish_string(content, **self.rst_kw)
 
 
+class Jinja2(Filter):
+    # FIXME: This is untested and probably not Best Practices compliant. Someone
+    # who seriously uses Jinja2 should make this filter better.
+
+    # TODO: Make a Jinja2Container version of this Filter (similar to MakoContainer)
+
+    def __init__(self, index, searchpaths=None):
+        super(Jinja2, self).__init__(index)
+
+        if not jinja2:
+            raise ImportError("Jinja2 filter requires the 'Jinja2' package to be installed.")
+
+        loaders = []
+        if searchpaths:
+            loaders.append(jinja2.FileSystemLoader(searchpaths))
+
+        # TODO: Add support for more loaders?
+
+        self.jinja_env = jinja2.Environment(
+            loaders=jinja2.ChoiceLoaders(loaders)
+        )
+
+    def __call__(self, content, route=None):
+        t = self.jinja_env.from_string(content)
+        return t.render(index=self.index, route=route)
+
+
+
 default_filters = {
     'mako': Mako,
     'markdown': Markdown,
     'rst': RestructuredText,
+    'jinja2': Jinja2,
 }
